0d5a4682c4d7ef0d8b07533481da61806541d9ca
MackRoe <elaine.music@students.makeschool.com>
2020-05-03 14:41:13 -0700

    breakout problem 1 rough solution

---
+++ PracticeProblem1.py
@@ -0,0 +1,9 @@
+def check_for_dupes(num_list):
+	current_n = num_list(0)
+	for i in range(1, (len(num_list) - 1))
+next_n = num_list(i)
+if next_n > current_n:
+	greatest_n = next_n
+else:
+	greatest_n = current_n
+return greatest_n
